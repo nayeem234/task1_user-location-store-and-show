6be5ace9d1626ab2bdb0f53028064c24bd5cbd9c
unknown <nayeemsarwar1275@gmail.com>
2022-11-27 11:01:24 +0600

    my changes

--- InsertDataWithGeoLocationData/add.php
+++ InsertDataWithGeoLocationData/add.php
@@ -2,24 +2,19 @@
 
 require 'connection.php';
 
-if(isset($_POST["SUBMIT"])){
-    $name=$_POST["name"];
-    $email=$_POST["email"];
-    $latitude=$_POST["latitude"];
-    $longitude=$_POST["longitude"];
+if (isset($_POST["submit"])) {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $latitude = $_POST["latitude"];
+  $longitude = $_POST["longitude"];
 
-    $query="INSERT INTO tbl_data VALUES('','$name','$email','$latitude','$longitude')";
-    mysqli_query($conn,$query);
-    echo
-    "
+  $query = "INSERT INTO tbl_data VALUES('','$name','$email','$latitude','$longitude')";
+  mysqli_query($conn, $query);
+  echo
+  "
       <script>
       alert('Data added successfully');
       document.location.href='data.php';
       </script>
-      "
-      ;
-      
-
-
+      ";
 }
-?>
--- InsertDataWithGeoLocationData/index.php
+++ InsertDataWithGeoLocationData/index.php
@@ -1,9 +1,8 @@
-
-
 <!DOCTYPE html>
 
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,56 +11,53 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
+
 <body onload="getLocation();">
 
-<form class="myForm" action="add.php" method="post" autocomplete="off">
+    <form class="myForm" action="add.php" method="post" autocomplete="off">
 
-<label for="">Name</label>
-<input type="text" name="name" required value=""><br>
+        <label for="">Name</label>
+        <input type="text" name="name" required value=""><br>
 
 
-<label for="">Email</label>
-<input type="text" name="email" required value=""><br>
+        <label for="">Email</label>
+        <input type="text" name="email" required value=""><br>
 
-<input type="hidden" name="latitude" value="">
+        <input type="hidden" name="latitude"  id="latitude" value="">
 
-<input type="hidden" name="longitude" value=""><br>
+        <input type="hidden" name="longitude" id="longitude" value=""><br>
 
-<button type="submit" name="submit">submit</button>
+        <button type="submit" name="submit">submit</button>
 
-</form>
+    </form>
 
-<script type="text/javascript">
-        function getLocation () { 
-            if(navigator.geolocation){
-                navigator.geolocation.getCurrentPosition(showPosition,showError);
+    <script type="text/javascript">
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
 
             }
-         }
+        }
 
-         function showPosition(position){
-            document.querySelector('.myForm input[name="latitude"]').value=position.coords.latitude;
-            document.querySelector('.myForm input[name="longitude"]').value=position.coords.longitude;
+        function showPosition(position) {
+            document.querySelector('#latitude').value = position.coords.latitude;
+            document.querySelector('#longitude').value = position.coords.longitude;
 
-         }
-         function showError(error){
-            switch(error.code){
+        }
+
+        function showError(error) {
+            switch (error.code) {
                 case error.PERMISSION_DENIED:
-                alert("you must Allow The request for Geolocation To Fill Out the form");
-                location.reload();
-                break;
+                    alert("you must Allow The request for Geolocation To Fill Out the form");
+                    location.reload();
+                    break;
             }
 
-         }
-
+        }
     </script>
     <br>
     <a href="data.php">Database Page</a>
-    
+
 </body>
+
 </html>
-
-
-
-
-
